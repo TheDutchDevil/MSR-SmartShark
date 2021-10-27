7153e500ddb90f5c0f0dc6146fedd2afec4c93ae
Nathan Cassee <n.w.cassee@tue.nl>
2021-10-27 10:23:25 +0200

    Add files for the full version

--- full/setup.sh
+++ full/setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "Hello world"
+wget -O /tmp/shark.agz https://data.goettingen-research-online.de/api/access/datafile/:persistentId?persistentId=doi:10.25625/7OZ1SP/CUOQ6K
+
+mongorestore --gzip --archive=/tmp/shark.agz
+
+echo "Restored smartshark DB"
+
+rm /tmp/shark.agz
