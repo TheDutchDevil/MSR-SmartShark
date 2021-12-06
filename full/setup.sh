a6beccb5fbd8718ddb09d73677de71a1cfa8478e
Nathan Cassee <n.w.cassee@tue.nl>
2021-12-06 12:19:07 +0100

    Make file download more robus

--- full/setup.sh
+++ full/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -O /tmp/shark.agz https://data.goettingen-research-online.de/api/access/datafile/:persistentId?persistentId=doi:10.25625/7OZ1SP/CUOQ6K
+wget --continue --progress=do:mega --tries=0 -O /tmp/shark.agz https://data.goettingen-research-online.de/api/access/datafile/:persistentId?persistentId=doi:10.25625/7OZ1SP/CUOQ6K
 
 mongorestore --gzip --archive=/tmp/shark.agz
 
