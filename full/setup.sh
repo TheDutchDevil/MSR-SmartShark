e06f36816b85954e9b9f5b03262f1ffcd3e0af6e
Nathan Cassee <n.w.cassee@tue.nl>
2021-12-06 12:50:31 +0100

    Different Download source

--- full/setup.sh
+++ full/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget --continue --progress=do:mega --tries=0 -O /tmp/shark.agz https://data.goettingen-research-online.de/api/access/datafile/:persistentId?persistentId=doi:10.25625/7OZ1SP/CUOQ6K
+wget --continue --progress=do:giga --tries=0 -O /tmp/shark.agz http://141.5.100.155/smartshark_2_1.agz
 
 mongorestore --gzip --archive=/tmp/shark.agz
 
